49601bab29c2b0a727eca496344e778eb907f245
DATA <data.delete3509@gmail.com>
2026-01-30 19:37:42 +0300

    DBonDemo 1.1v

--- Data's Book on Demo/Sources/JS/setBackgroundMedia.js
+++
@@ -1,4 +0,0 @@
-if(true)
-{
-    document.write("<div class='video-container'><video autoplay muted loop id='arkaplanVideo'><source src='../Media/video/STARFALL.mp4' type='video/mp4'>Tarayıcınız video oynatmayı desteklemiyor.</video></div>");
-}
---
+++ MainWindow.axaml.cs
@@ -0,0 +1,51 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using Avalonia.Threading;
+using System.Threading;
+using System.Threading.Tasks;
+using Xilium.CefGlue;
+
+namespace Data_s_Book_on_Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(100);
+
+                    Dispatcher.UIThread.InvokeAsync(() =>
+                    {
+                        WebSiteTitle.Content = Browser.Title;
+                        Title = Browser.Title;
+                    });
+                }
+            });
+
+            Browser.Address = "file:///Data's%20Book%20on%20Demo/Lessons/Table%20Of%20Contents.html"; // Data's Demo Book url
+            FaviconImage.Source = new Bitmap("Data's Book on Demo/Sources/favicon.png"); // Data's Demo Book icon
+
+            Icon = new WindowIcon(new Bitmap("Data's Book on Demo/Sources/favicon.png")); // Data's Demo Book icon
+        }
+
+        private void GoBackWebView(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (Browser.CanGoBack == true)
+            {
+                Browser.GoBack();
+            }
+        }
+        private void GoForwardWebView(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (Browser.CanGoForward == true)
+            {
+                Browser.GoForward();
+            }
+        }
+    }
+}
